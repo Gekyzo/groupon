799f6290f2236d14deff3a432656ac121658ca49
Ciro Mora <ciro.mora@gmail.com>
2019-03-28 16:24:07 +0100

    Componente UploadImage crea las carpetas necesarias

--- src/Controller/Component/UploadImageComponent.php
+++ src/Controller/Component/UploadImageComponent.php
@@ -1,8 +1,12 @@
 <?php
 namespace App\Controller\Component;
 
+use Cake\Core\Configure;
 use Cake\Controller\Component;
 use Cake\Controller\ComponentRegistry;
+use Cake\Filesystem\Folder;
+use Cake\Filesystem\File;
+use Cake\Log\Log;
 
 /**
  * Uploadimage component
@@ -16,4 +20,48 @@ class UploadimageComponent extends Component
      * @var array
      */
     protected $_defaultConfig = [];
+
+    /**
+     * Listado de carpetas necesarias para almacenar imágenes de la aplicación
+     * @var array
+     */
+    private $requiredFolders = ['categories', 'promotions'];
+
+    /**
+     * Método principal
+     */
+    public function mainUpload()
+    {
+        self::createFolders();
+    }
+
+    /**
+     * Funciones privadas a ejecutar por mainUpload()
+     */
+
+    /**
+     * Comprobar si existe una determinada carpeta a través del atributo 'path' de * la clase de Cake 'Folder'
+     */
+    private function checkFolderExists($folder)
+    {
+        if ($folder->path) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Creo las carpetas que require la aplicación para su funcionamiento
+     */
+    private function createFolders()
+    {
+        foreach ($this->requiredFolders as $folder) {
+            $newFolderDir = Configure::read('Fol.images') . $folder . '\\';
+            $folderDir = new Folder($newFolderDir);
+            if (!self::checkFolderExists($folderDir)) {
+                Log::info('Creación de la carpeta ' . $newFolderDir, ['dirCreation']);
+                $folderDir->create($newFolderDir);
+            }
+        }
+    }
 }
